64f8f1476d774a27d8dab86003a7c4823eb8ddee
Laura Webber <laura@hightouch.io>
2022-02-02 14:38:14 -0800

    update js

--- house-project/index.js
+++ house-project/index.js
@@ -132,8 +132,7 @@ class DOMManager {
                     `<p>
                     <span id="name-${room._id}"><strong>Name: </strong> ${room.name}</span>
                     <span id="area-${room._id}"><strong>Area: </strong> ${room.area}</span>
-                    <button class="btn btn-danger" onclick="DOMManager.deleteRoom('${house._id}', '${room._id}')">Delete Room</button>
-                    </p>`
+                    <button class="btn btn-danger" onclick="DOMManager.deleteRoom('${house._id}', '${room._id}')">Delete Room</button>`
                 );
             }
         }
